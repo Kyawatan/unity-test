4b493c3e07c935978e11b3a82d23a423000a2443
Kyawatan <nico.vip@icloud.com>
2021-08-16 05:06:37 +0900

    first commit.

---
+++ ClimbCloud/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        this.player = GameObject.Find("cat");
+    }
+
+    void Update()
+    {
+        //プレイヤーの座標を調べる
+        Vector3 playerPos = this.player.transform.position;
+        //カメラのY座標に反映
+        transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+    }
+}
