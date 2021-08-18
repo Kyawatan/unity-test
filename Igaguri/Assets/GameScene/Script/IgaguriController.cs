f016445742dfb2581d76f6f387c802dc596faeea
Kyawatan <nico.vip@icloud.com>
2021-08-19 01:59:32 +0900

    イガグリを飛ばして的に当てるゲームを作成。

---
+++ Igaguri/Assets/GameScene/Script/IgaguriController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        //イガグリに力を加える
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void Start()
+    {
+        //Shoot(new Vector3(0, 300, 1500));
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //イガグリの動作を停止させる
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
---
+++ Igaguri/Assets/GameScene/Script/IgaguriGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+    public GameObject m_igaguriPrefab;
+
+    void Update()
+    {
+        //マウスクリックするとイガグリのインスタンス生成
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(m_igaguriPrefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+
+            //タップした位置に向けてイガグリを飛ばす
+            igaguri.GetComponent<IgaguriController>().Shoot(
+                worldDir.normalized * 1500);
+        }
+    }
+}
