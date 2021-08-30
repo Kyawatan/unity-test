d3e396469cfbd78cc50ab686c9baa3748cfb3cc0
Kyawatan <nico.vip@icloud.com>
2021-08-30 09:57:01 +0900

    プレイヤーと敵キャラクターを設置。敵キャラクターがプレイヤーを追いかける。

---
+++ 3DAction/Assets/GameScene/Scripts/CollisionDerector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Collider))]
+
+public class CollisionDerector : MonoBehaviour
+{
+    //[SerializeField] private TriggerEvent m_onTriggerStay = new TriggertEvent();
+
+    void OnTriggerStay(Collider other)
+    {
+        //指定された処理を実行する
+        //m_onTriggerStay.Invoke(other);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ 3DAction/Assets/GameScene/Scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] private PlayerController m_playerController;
+    private NavMeshAgent m_agent;
+
+    void Start()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //プレイヤーを目指して進む
+        m_agent.destination = m_playerController.transform.position;
+    }
+}
